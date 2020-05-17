8fc82782d893ab440ad42c5c04ad53f203f3dcfd
George Okumu <gokumu12@gmail.com>
2020-05-17 10:03:03 +0300

    made the what we do icons and its description to fadeIn and fadeOut

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
     $("#design-image").click(function(){
-      $("#design-image").slideDown("1500").hide("1000");
-      $("#design").show();
+      $("#design-image").fadeIn("2000").hide("2000");
+      $("#design").show("2000");
     });
     $("#design").click(function(){
-      $("#design").fadeOut("1500");
-      $("#design-image").slideDown("1500");
+      $("#design").fadeOut("2000");
+      $("#design-image").fadeIn("2000");
     });
   });
 
   $(document).ready(function(){
     $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
+      $("#development-image").fadeIn("2000").hide("2000");
+      $("#development").show();
     });
     $("#development").click(function(){
-      $("#development").fadeOut('1500');
-      $("#development-image").slideDown('1500');
+      $("#development").fadeOut("2000");
+      $("#development-image").fadeIn("2000");
     });
   });
 
   $(document).ready(function(){
     $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
+      $("#product-image").fadeIn().hide("2000");
+      $("#product").show("2000");
     });
     $("#product").click(function(){
-      $("#product").fadeOut('1500');
-      $("#product-image").slideDown('1500');
+      $("#product").fadeOut("2000");
+      $("#product-image").fadeIn("2000");
     });
   });
